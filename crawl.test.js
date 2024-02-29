78906e2d4887af5154f0f12cf9f6b610e04cbcf5
Meghna Chakraborty <meghnachakraborty12@gmail.com>
2024-02-29 09:17:37 +0530

    add 4 test-cases

--- crawl.test.js
+++ crawl.test.js
@@ -1,8 +1,29 @@
 const {normalizeURL} = require('./crawl.js')
 const {test,expect} = require('@jest/globals')
 
-test('normalizeURL', () => {
+test('normalizeURL strip protocol', () => {
     const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected) //PASS
+})
+
+test('normalizeURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected) //PASS
+})
+
+test('normalizeURL capitals', () => {
+    const input = 'https://Blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected) //PASS
+})
+
+test('normalizeURL strip http', () => {
+    const input = 'https://Blog.boot.dev/path'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected) //PASS
